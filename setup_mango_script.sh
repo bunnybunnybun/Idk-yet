b9036e3f4b792f1b5e30fab19ab9f282c07454d7
Carlisle Poe <carlislepoe@gmail.com>
2025-11-05 18:30:22 -0800

    Set animations for Fuzzel launcher. You can now use JKL; keybinds to move floating windows. Tweaked some minor Mango config variables.

--- setup_mango_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp ~/this_distro_thingy/mango_stuff/config.conf ~/.config/mango/config.conf
-cp ~/this_distro_thingy/mango_stuff/autostart.sh ~/.config/mango/autostart.sh
---
+++ this_distro_thingy/setup_mango_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp ~/this_distro_thingy/this_distro_thingy/mango_stuff/config.conf ~/.config/mango/config.conf
+cp ~/this_distro_thingy/this_distro_thingy/mango_stuff/autostart.sh ~/.config/mango/autostart.sh
