3347dcaab3c891160897750980308c69a3649e5d
Carlisle Poe <carlislepoe@gmail.com>
2025-11-09 19:16:30 -0800

    Themed the Kitty terminal to fit the pink aesthetic!

--- this_distro_thingy/install_script.sh
+++ this_distro_thingy/install_script.sh
@@ -8,6 +8,7 @@ else
 fi
 yay -S --needed mangowc-git quickshell
 sudo cp -r ~/this_distro_thingy/this_distro_thingy/fuzzel /etc/xdg/
+cp -r ~/this_distro_thingy/this_distro_thingy/kitty ~/.config/
 mkdir ~/.config/mango
 chmod +x ~/Idk-yet/this_distro_thingy/setup_mango_script.sh
 ~/Idk-yet/this_distro_thingy/setup_mango_script.sh
