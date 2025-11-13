ee324d7ae4d48e2f3890c2bd5cc2d828ca0ff776
Carlisle Poe <carlislepoe@gmail.com>
2025-11-12 18:31:41 -0800

    Added a way to highlight your cursor if you lose it :P

--- Idk-yet/install_script.sh
+++ Idk-yet/install_script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo pacman -Syu
-sudo pacman -S --needed kitty git base-devel fuzzel swaybg sddm gtk3 gtk4
+sudo pacman -S --needed kitty git base-devel fuzzel swaybg sddm gtk3 gtk4 gtk-layer-shell
 sudo systemctl enable sddm
+pip install PyGOBject --break-system-packages
 if [ -f "$HOME/Idk-yet/.yay_has_been_installed" ]; then
     echo "Yay has already been installed"
 else
@@ -14,3 +15,4 @@ cp -r ~/Idk-yet/Idk-yet/kitty ~/.config/
 mkdir ~/.config/mango
 chmod +x ~/Idk-yet/Idk-yet/setup_mango_script.sh
 ~/Idk-yet/Idk-yet/setup_mango_script.sh
+git clone https://github.com/cjacker/wl-find-cursor && cd wl-find-cursor && make
